f8c7b1ee18eb6359888c0219a7525caadf2a734b
Bruno Lages <brunok1lages@gmail.com>
2022-11-19 19:44:27 -0300

    feat: styled components non-funcional

--- src/CreateMusic/CreateMusic.js
+++ src/CreateMusic/CreateMusic.js
@@ -1,37 +1,49 @@
 import React from "react";
-import "./CreateMusic.css";
+import {
+    Title,
+    CreateMusic,
+    BpmSelector,
+    
+} from "./Styles";
 
 function createMusic() {
     return (
-        <body>
-            <div id="createMusic">
-                <div className="musicCreation">
-                    <strong>CRIAÇÃO DA MÚSICA</strong>
-                    <form>
-                        <div id="fields">
-                            <label>Título: </label>
-                            <input type="text" className="título"></input><br />
-                        </div>
-                        <div id="fields">
-                            <label>Autor: </label>
-                            <input type="text" className="autor"></input><br />
-                        </div>
-                        <div id="fields">
-                            <label>BPM: </label>
-                            <input type="text" className="bpm"></input>
-                        </div>
-                    </form >
-                    <select name="select">
-                        <option value="valor1" selected> Tamanho do passo</option>
-                        <option value="valor1">3 divisões</option>
-                        <option value="valor2">4 divisões</option>
-                    </select>
-                </div >
-                <button>
-                    Nova Batida
-                </button>
-            </div>
-        </body >
+        // <body>
+        //     <div id="createMusic">
+        //         <div className="musicCreation">
+        //             <strong>CRIAÇÃO DA MÚSICA</strong>
+        //             <form>
+        //                 <div id="fields">
+        //                     <label>Título: </label>
+        //                     <input type="text" className="título"></input><br />
+        //                 </div>
+        //                 <div id="fields">
+        //                     <label>Autor: </label>
+        //                     <input type="text" className="autor"></input><br />
+        //                 </div>
+        //                 <div id="fields">
+        //                     <label>BPM: </label>
+        //                     <input type="text" className="bpm"></input>
+        //                 </div>
+        //             </form >
+        //             <select name="select">
+        //                 <option value="valor1" selected> Tamanho do passo</option>
+        //                 <option value="valor1">3 divisões</option>
+        //                 <option value="valor2">4 divisões</option>
+        //             </select>
+        //         </div >
+        //         <button>
+        //             Nova Batida
+        //         </button>
+        //     </div>
+        // </body >
+
+        <div>
+            <CreateMusic>
+                <Title> CRIAÇÃO DA MÚSICA </Title>
+                <BpmSelector></BpmSelector>
+            </CreateMusic>
+        </div>
     )
 }
 
